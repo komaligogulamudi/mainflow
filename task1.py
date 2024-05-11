7124ac6475101b5b109a032e0057deb2118d9ca3
komaligogulamudi <komalikomali073@gmail.com>
2024-05-11 13:33:38 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,23 @@
+# Create, add an element, remove an element, modify an element from  a list
+
+my_list = [1, 2, 3, 4, 5]
+my_list.append(6)
+my_list.remove(3)
+my_list[0] = 10
+print("List:")
+print(my_list)
+
+# Create, add an element, remove an element, modify an element from  a dictionary
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+my_dict['d'] = 4
+del my_dict['b']
+my_dict['a'] = 100
+print("Dictionary:")
+print(my_dict)
+# create,add an element, remove an element,modify an element from a set
+my_set={1,2,3,4,5}
+my_set.add(6)
+my_set.remove(3)
+print("set:")
+print(my_set)
